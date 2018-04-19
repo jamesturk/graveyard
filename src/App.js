408a661ab9f22588eab2ded2e3f6da3d09b2ad45
James Turk <james.p.turk@gmail.com>
2018-04-19 12:49:22 -0400

    basic chi-squared display

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ class DiceTableRow extends Component {
   }
 }
 
+// map degrees-of-freedom (sides-1) to 90, 95, 97.5 and 99% thresholds
+const CHI_SQUARED_CRITICAL = {
+  3: [6.251, 7.815, 9.348, 11.345],
+  5: [9.236, 11.070, 12.833, 15.086],
+  7: [12.017, 14.067, 16.013, 18.475],
+  9: [14.684, 16.919, 19.023, 21.666],
+  11: [17.275, 19.675, 21.920, 24.725],
+  19: [27.204, 30.144, 32.852, 36.191],
+}
+
 class DiceTable extends Component {
   constructor(props) {
     super(props);
@@ -33,6 +43,7 @@ class DiceTable extends Component {
       rows.push(this.renderRow(i));
     }
     return (
+      <div>
       <table>
         <thead>
           <tr><th>#</th><th>count</th></tr>
@@ -41,6 +52,8 @@ class DiceTable extends Component {
           {rows}
         </tbody>
       </table>
+      Computed a X<sup>2</sup> of <i>{this.chiSquared()}</i>, probability of bad dice <i>{this.chiSquaredPassage()}</i>
+      </div>
     );
   }
 
@@ -48,6 +61,41 @@ class DiceTable extends Component {
     var newState = this.state;
     newState.counts[i]++;
     this.setState(newState);
+  }
+
+  chiSquared() {
+    var total = 0;
+    for(var i=0; i < this.props.num_sides; i++) {
+      total += this.state.counts[i];
+    }
+
+    var expected = total / this.props.num_sides;
+
+    var chiSquared = 0;
+
+    for(i=0; i < this.props.num_sides; i++) {
+      chiSquared += (this.state.counts[i] - expected) ** 2 / expected;
+    }
+
+    return chiSquared;
+  }
+
+  chiSquaredPassage() {
+    var table = CHI_SQUARED_CRITICAL[this.props.num_sides-1];
+
+    var chiVal = this.chiSquared();
+
+    if(chiVal < table[0]) {
+      return '90%';
+    } else if(chiVal < table[1]) {
+      return '95%';
+    } else if(chiVal < table[1]) {
+      return '97.5%';
+    } else if(chiVal < table[4]) {
+      return '99%';
+    } else {
+      return '100%';
+    }
   }
 }
 
