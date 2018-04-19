611aafee6952fdfe23de0eef2aa57a8c10318920
James Turk <james.p.turk@gmail.com>
2018-04-19 12:15:05 -0400

    button state working

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ class DiceTableRow extends Component {
   render() {
     return (
       <tr>
-        <td><button name="{this.props.n}" onClick={this.click}>{this.props.n}</button></td>
+        <td><button name="{this.props.n}" onClick={this.props.click}>{this.props.n}</button></td>
         <td>{this.props.count}</td>
       </tr>
     );
   }
-
-  click() {
-    this.props.count++;
-  }
 }
 
 class DiceTable extends Component {
+  constructor(props) {
+    super(props);
+    var counts = [];
+
+    for(var i=0; i < this.props.num_sides; i++) {
+      counts.push(0);
+    }
+
+    this.state = {'counts': counts};
+  }
+
+  renderRow(i) {
+    return (<DiceTableRow n={i} count={this.state.counts[i]} click={() => this.click(i)} key={i} />);
+  }
+
   render() {
     var rows = [];
-    for(var i=1; i <= this.props.num_sides; i++) {
-      rows.push(<DiceTableRow n={i} />);
+    for(var i=0; i < this.props.num_sides; i++) {
+      rows.push(this.renderRow(i));
     }
     return (
       <table>
@@ -31,6 +42,12 @@ class DiceTable extends Component {
         </tbody>
       </table>
     );
+  }
+
+  click(i) {
+    var newState = this.state;
+    newState.counts[i]++;
+    this.setState(newState);
   }
 }
 
