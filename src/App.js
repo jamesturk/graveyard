607711f443b9b1448baf8774b78b203868807fad
James Turk <james.p.turk@gmail.com>
2018-05-14 14:08:44 -0400

    add concept of pages / more flexibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import dataBlocks from './data.js';
 
 class InfoBlock extends Component {
@@ -42,14 +41,58 @@ class InfoBlock extends Component {
   }
 }
 
-class App extends Component {
+class Page extends Component {
+  render() {
+    const blocks = this.props.blocks.map((b, i) => <InfoBlock type={b} key={i} />);
+    return (
+      <div className="page sheet">
+        {blocks}
+      </div>
+    )
+  }
+}
+
+class Controls extends Component {
   render() {
     return (
+      <div className="controls">
+        <button id="addPage" onClick={this.props.addPage}>Add Page</button>
+        <button id="removePage" onClick={this.props.removePage}>Remove Page</button>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages: [
+        ['actions', 'exhaustion'],
+        ['lifestyle'],
+      ]
+    };
+    
+    this.addPage = this.addPage.bind(this);
+    this.removePage = this.removePage.bind(this);
+  }
+
+  render() {
+    const pages = this.state.pages.map((p, i) => <Page blocks={p} key={i} />);
+    return (
       <div className="App">
-        <InfoBlock type="actions" />
-        <InfoBlock type="exhaustion" />
+        <Controls addPage={this.addPage} removePage={this.removePage} />
+        {pages}
       </div>
     );
+  }
+
+  addPage() {
+    this.state.pages.push([]);
+  }
+
+  removePage() {
+    this.state.pages.pop();
   }
 }
 
